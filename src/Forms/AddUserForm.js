db1c73d3280d0f2efc99fb165c81e86ddb52aedc
HelenDaisyJohn <helendaisy@bloomlync.com>
2023-11-22 15:57:58 +0530

    Code Formated

--- src/Forms/AddUserForm.js
+++ src/Forms/AddUserForm.js
@@ -1,35 +1,39 @@
 import { useState } from "react";
 
+const AddUserForm = (props) => {
+  const initialFormState = { id: null, name: "", username: "" };
+  const [user, setUser] = useState(initialFormState);
 
-const AddUserForm = (props) =>
-{
-    const initialFormState ={id:null, name:'', username:'' }
-    const[user, setUser]= useState(initialFormState);
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
 
-    const handleInputChange =(event) =>
-    {
-        const{name,value}=event.target
-        setUser({...user,[name]:value})
-    }
-
-
-    return (
-    <form onSubmit={
-        event =>
-        {
-            event.preventDefault();
-            if(!user.name|| !user.username) return;
-            props.addUser(user);
-            setUser(initialFormState);
-        }
-
-    }>
-        <label> Name </label>
-        <input type="text" onChange={handleInputChange} name ="name" value={user.name}/>
-        <label> User Name </label>
-        <input type="text" onChange={handleInputChange} name ="username" value={user.username}/>
-        <button>Add New User</button>
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (!user.name || !user.username) return;
+        props.addUser(user);
+        setUser(initialFormState);
+      }}
+    >
+      <label> Name </label>
+      <input
+        type="text"
+        onChange={handleInputChange}
+        name="name"
+        value={user.name}
+      />
+      <label> User Name </label>
+      <input
+        type="text"
+        onChange={handleInputChange}
+        name="username"
+        value={user.username}
+      />
+      <button>Add New User</button>
     </form>
-    )
-}
+  );
+};
 export default AddUserForm;
